c901e05c9e0e020ddcc5c83c80144dfda6cb46b9
Jae Chung <jaepublic@gmail.com>
2021-03-18 15:43:12 -0500

    chore: add action type and thunk for posting new item

---
+++ client/store/item-jae.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const CREATE_NEW_ITEM = 'CREATE_NEW_ITEM'
+
+export const createNewItem = (item) => {
+  return {
+    type: CREATE_NEW_ITEM,
+    item,
+  }
+}
+
+export const postNewItem = (item) => {
+  return async (dispatch) => {
+    try {
+      const {data} = axios.post(`/api/users/post`, item)
+      console.log('hello', 'in postNewItem thunk', {item, data})
+      dispatch(createNewItem(data))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
