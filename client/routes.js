e9102cdf68399d92a083a0e7b1d96125ae840e2d
Jae Chung <jaepublic@gmail.com>
2021-03-18 14:30:39 -0500

    chore: render new item form upon vising /post

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, MapExp} from './components'
+import {Login, Signup, UserHome, MapExp, NewItemForm} from './components'
 import {me} from './store'
 
 /**
@@ -26,6 +26,7 @@ class Routes extends Component {
           <Switch>
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
+            <Route path="/post" component={NewItemForm} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
