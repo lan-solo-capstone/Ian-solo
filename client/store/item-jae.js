a859f500e62be6e5450b3e3bc5db57691364285d
Jae Chung <jaepublic@gmail.com>
2021-03-18 15:47:14 -0500

    chore: add subreducer for single item

--- client/store/item-jae.js
+++ client/store/item-jae.js
@@ -12,11 +12,21 @@ export const createNewItem = (item) => {
 export const postNewItem = (item) => {
   return async (dispatch) => {
     try {
-      const {data} = axios.post(`/api/users/post`, item)
+      const {data} = await axios.post(`/api/users/post`, item)
       console.log('hello', 'in postNewItem thunk', {item, data})
       dispatch(createNewItem(data))
     } catch (err) {
       console.error(err)
     }
+  }
+}
+
+const initialState = {}
+export default function itemReducer(state = initialState, action) {
+  switch (action.type) {
+    case CREATE_NEW_ITEM:
+      return action.item
+    default:
+      return state
   }
 }
