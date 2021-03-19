0c1a38272440211c215218fd91349c29a880f568
Jae Chung <jaepublic@gmail.com>
2021-03-18 21:43:34 -0500

    refactor: change id to name in form fields

--- client/components/NewItemForm.js
+++ client/components/NewItemForm.js
@@ -2,8 +2,23 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {postNewItem} from '../store/item-jae.js'
 
+const initialState = {itemName: '', itemDescription: ''}
 class NewItemForm extends Component {
+  constructor() {
+    super()
+    this.state = initialState
+    this.handleChange = this.handleChange.bind(this)
+    // this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    })
+  }
+
   render() {
+    console.log('heeeey!', this.state)
     return (
       <div className="container-fluid">
         <div className="row">
@@ -14,7 +29,7 @@ class NewItemForm extends Component {
                   <label className="mr-sm-2" htmlFor="postType">
                     What type of post is this?
                   </label>
-                  <select className="custom-select mr-sm-2" id="postType">
+                  <select className="custom-select mr-sm-2" name="postType">
                     <option selected>Choose...</option>
                     <option value="Offer">It&apos;s an OFFER of an item</option>
                     <option value="Seeking">
@@ -26,7 +41,13 @@ class NewItemForm extends Component {
               <div className="row">
                 <div className="form-group">
                   <label htmlFor="itemName">Item Name</label>
-                  <input type="text" className="form-control" id="itemName" />
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="itemName"
+                    value={this.state.itemName}
+                    onChange={this.handleChange}
+                  />
                 </div>
               </div>
               <div className="row">
@@ -34,8 +55,10 @@ class NewItemForm extends Component {
                   <label htmlFor="itemDescription">Item Description</label>
                   <textarea
                     className="form-control"
-                    id="itemDescription"
                     rows="5"
+                    name="itemDescription"
+                    value={this.state.itemDescription}
+                    onChange={this.handleChange}
                   />
                 </div>
               </div>
@@ -44,7 +67,10 @@ class NewItemForm extends Component {
                   <label className="mr-sm-2" htmlFor="itemCondition">
                     Condition of your item
                   </label>
-                  <select className="custom-select mr-sm-2" id="itemCondition">
+                  <select
+                    className="custom-select mr-sm-2"
+                    name="itemCondition"
+                  >
                     <option selected>Choose...</option>
                     <option value="New">Like New</option>
                     <option value="Gently_Used">Gently Used</option>
@@ -60,7 +86,7 @@ class NewItemForm extends Component {
                     // need to add other file types to support
                     accept="image/x-png,image/jpeg,image/gif"
                     className="form-control-file"
-                    id="uploadPhoto"
+                    name="uploadPhoto"
                     multiple
                   />
                 </div>
