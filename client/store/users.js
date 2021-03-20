29a0a52bac9a79732c1db879d3681643bc649473
Jae Chung <jaepublic@gmail.com>
2021-03-19 21:45:40 -0500

    chore: add redux store for all users

---
+++ client/store/users.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const GET_USERS = 'GET_USERS'
+
+const getUsers = (users) => ({type: GET_USERS, users})
+
+export const fetchUsers = async (dispatch) => {
+  try {
+    const {data} = await axios.get('/api/users')
+    dispatch(getUsers(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const initialState = []
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_USERS:
+      return action.users
+    default:
+      return state
+  }
+}
