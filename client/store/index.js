aec047f176c7b3dcd4c122d25f66eaebd4ed7857
Jae Chung <jaepublic@gmail.com>
2021-03-19 21:55:28 -0500

    chore: add users subreducer to reducer

--- client/store/index.js
+++ client/store/index.js
@@ -3,9 +3,10 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import users from './users'
 import listAll from './listAll'
 
-const reducer = combineReducers({user, listAll})
+const reducer = combineReducers({user, users, listAll})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
