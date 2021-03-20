194926e6f11055238ba5009d235f6eab12596312
Jae Chung <jaepublic@gmail.com>
2021-03-20 16:56:56 -0500

    chore: add boilerplate for User component

---
+++ client/components/User.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {me, fetchExistingUser} from '../store/user.js'
+import {UserView} from './index.js'
+
+class User extends Component {
+  componentDidMount() {
+    this.props.fetchUser()
+    this.props.fetchExistingUser()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.user.id && this.props.user.id) this.props.fetchExistingUser()
+  }
+
+  render() {
+    console.log('in User render, this.props', this.props)
+    return <div>test</div>
+  }
+}
+
+const mapState = (state) => {
+  return {
+    user: state.user,
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    fetchUser: () => dispatch(me()),
+    fetchExistingUser: (userId) => dispatch(fetchExistingUser(userId)),
+  }
+}
+
+export default connect(mapState, mapDispatch)(User)
