6c56c52f988d84810d5acfefb8bf27d8f89c64ac
Jae Chung <jaepublic@gmail.com>
2021-03-19 21:25:06 -0500

    chore: update attributes in GET route for all users

--- server/api/users.js
+++ server/api/users.js
@@ -4,13 +4,25 @@ module.exports = router
 
 // /api/users
 router.use('/post', require('./newitemform.js'))
+
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email'],
+      attributes: [
+        'id',
+        'email',
+        'firstName',
+        'middleName',
+        'lastName',
+        'street1',
+        'street2',
+        'city',
+        'state',
+        'zip',
+      ],
     })
     res.json(users)
   } catch (err) {
