ec3f67aff37a1bfa13edc4d2dc327e1e4042b0a1
MelindaJoArmbruster <melinda@armbrustermail.com>
2021-03-21 09:59:47 -0500

    POC for Geocode API call

--- client/components/temp.js
+++ client/components/temp.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import {MapSingleItem, MapAllItems} from '../components'
+import axios from 'axios'
 
 //the object format is expected to change per Yoshie and I will address
 //that inside both map components. For now, if you link to the map components
 //and pass on props as shown below line 35, I will handle the rest.
 const temp = () => {
+  useEffect(() => {
+    async function fetchData() {
+      const address = encodeURIComponent('142 Lions Ct, lake zurich, IL, 60047')
+      const data = (
+        await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?types=address&limit=1&access_token=pk.eyJ1IjoibWVsaW5kYWFybWJydXN0ZXIiLCJhIjoiY2trZTd6cHVlMDl5YzJwcXNvMWRvOHU4ciJ9.NdVU55Xhn75BzaVNjACSKQ`
+        )
+      ).data
+      if (data && data.features[0] && data.features[0].center) {
+        console.log(
+          data.features[0].center[1] + ', ' + data.features[0].center[0]
+        )
+      } else {
+        console.log(data)
+        console.log("ERROR: Can't find address!")
+      }
+      // console.log(data.data.features)
+    }
+
+    fetchData()
+  }, [])
+
   const itemsArray = [
     {
       id: 1,
