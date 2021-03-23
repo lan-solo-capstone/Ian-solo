4b4899d531e7652fd3555ceb29ce11edc4e14d4f
Jae Chung <jaepublic@gmail.com>
2021-03-22 21:38:08 -0500

    docs: add note to render unauthorized message

--- server/api/middleware.js
+++ server/api/middleware.js
@@ -1,19 +1,26 @@
+/* eslint-disable no-warning-comments */
 // this file contains authentication middlware to secure api routes
 
 const User = require('../db/models/user')
 
 function ensureAdmin(req, res, next) {
-  if (req.user && req.user.adminStatus) {
+  console.log('hello', 'in ensureAdmin req.user', req.user)
+  console.log('hello', 'hello', 'in ensureAdmin req.user.admin', req.user.admin)
+
+  if (req.user && req.user.admin) {
     next()
   } else {
+    // TODO: this is sending the 403 but the front end is still displaying
+    // "Loading, or we have no users"
+    // need to figure out how to get the page to render with "unauthorized" -- JC 3/22/2021
     res.sendStatus(403)
   }
 }
 
 function ensureLogin(req, res, next) {
-  const {id} = req.user
+  const id = req.user.id
   const passportId = req.session.passport.user
-
+  console.log('hello', 'in ensureLogin', req.user, req.session.passport)
   if (id && id === passportId) {
     next()
   } else {
