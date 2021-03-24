0258734bf44c2574fc597ea06e24fab50b3c1a35
Jae Chung <jaepublic@gmail.com>
2021-03-24 12:05:49 -0500

    chore: add boilerplate for chat container

---
+++ client/components/chat/ChatContainer.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+import {connect} from 'react-redux'
+import Sidebar from './Sidebar'
+import Navbar from './Navbar'
+import MessagesList from './MessagesList'
+import {fetchMessages} from '../store'
+
+export class Main extends Component {
+  componentDidMount() {
+    this.props.loadMessages()
+  }
+
+  render() {
+    return (
+      <div>
+        <Sidebar />
+        <Navbar />
+        <main>
+          <Switch>
+            <Route path="/chat/:chatId" component={MessagesList} />
+            {/* <Redirect to="/channels/1" /> */}
+          </Switch>
+        </main>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  loadMessages: () => dispatch(fetchMessages()),
+})
+
+export default connect(null, mapDispatchToProps)(Main)
