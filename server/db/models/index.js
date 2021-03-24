61fca4282d156f9c0590cb5de8b1bfd21fe64fb8
Jae Chung <jaepublic@gmail.com>
2021-03-24 16:06:45 -0500

    chore: add associations for users and messages

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,7 +1,7 @@
 const User = require('./user')
 const Item = require('./item')
 const ItemPhoto = require('./itemPhoto')
-
+const Message = require('./message')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -22,8 +22,13 @@ Item.belongsTo(User)
 ItemPhoto.belongsTo(Item)
 Item.hasMany(ItemPhoto)
 
+// tentative schema for chat -- JC 03/24/21
+User.hasMany(Message, {through: 'Channel'})
+Message.belongsTo(User, {through: 'Channel'})
+
 module.exports = {
   User,
   Item,
   ItemPhoto,
+  Message,
 }
