1b1475659841e97bc56b2818f190a95784619183
diegoA-66 <abreugdiego@gmail.com>
2021-03-24 18:20:03 -0400

    DONE! :+1: :+1: :+1: :+1:

--- client/store/item.js
+++ client/store/item.js
@@ -14,35 +14,6 @@ export const createNewItem = (item) => {
 export const postNewItem = (item) => {
   return async (dispatch) => {
     try {
-      //append text data from the form
-      // const formData = new FormData()
-
-      // const imageArr = async (photos) => {
-      //   if (photos) {
-      //     let arr = []
-
-      //     for (let i = 0; i < photos.length; i++) {
-      //       // encode the file using the FileReader API
-      //       const reader = new FileReader()
-      //       reader.onloadend = () => {
-      //         arr.push({
-      // imageName: `${photos[i].name}${Math.floor(
-      //   Math.random() * 100000
-      // )}`,
-      //           fileData: reader.result,
-      //         })
-      //       }
-      //       reader.readAsDataURL(photos[i])
-      //     }
-      //     /* creating base64 data from uploaded file
-      //   {
-      //     formData.append(`${item.uploadPhoto[i].name}`, item.uploadPhoto[i])
-      //   }
-      //   */
-      //     return arr
-      //   }
-      // }
-
       let fileInfo = {
         itemType: item.itemType,
         itemListName: item.itemListName,
@@ -53,55 +24,34 @@ export const postNewItem = (item) => {
       }
 
       if (item.uploadPhoto) {
-        item.uploadPhoto.forEach((element) => {
-          const random = `/images/${element.name}${Math.floor(
-            Math.random() * 100000
-          )}`
+        const imageInfo = await Promise.all(
+          item.uploadPhoto.map(async (element) => {
+            const random = `/images/${element.name}${Math.floor(
+              Math.random() * 100000
+            )}`
 
-          storage
-            .ref(random)
-            .put(element)
-            .then((response) => {
-              try {
-                console.log(`Added file: ${element.name} to cloud`)
-                storage
-                  .ref(random)
-                  .getDownloadURL()
-                  .then((url) => {
-                    console.log(url)
-                    console.log(random)
-                    fileInfo.imageArr.push({cloudRef: random, downloadUrl: url})
-                  })
-              } catch (error) {
-                console.log('failed')
-              }
-            })
-        })
+            const cloudCreate = await storage.ref(random).put(element)
+
+            const url = await storage.ref(random).getDownloadURL()
+
+            return {
+              cloudRef: random,
+              downloadUrl: url,
+              photoTitle: element.name,
+            }
+          })
+        )
+        fileInfo.imageArr = imageInfo
       }
 
-      /*
-      formData.append('itemType', item.itemType)
-      formData.append('itemListName', item.itemListName)
-      formData.append('description', item.description)
-      formData.append('itemCondition', item.itemCondition)
-      formData.append('userId', item.user.id)
-      */
-
-      //append file data from the form - if item.uploadPhoto IS NOT null
-
-      // if (item.uploadPhoto) {
-
-      // }
-
-      console.log('fileInfo', fileInfo)
-
       // vv test vv  below axios call is for testing purpose - visualize formData vv //
-      axios
-        .post('https://httpbin.org/anything', fileInfo)
-        .then((res) => {
-          console.log(res)
-        })
-        .catch((err) => console.log(err))
+
+      // axios
+      //   .post('https://httpbin.org/anything', fileInfo)
+      //   .then((res) => {
+      //     console.log(res)
+      //   })
+      //   .catch((err) => console.log(err))
 
       // ^^ test ^^//
 
--- server/api/items.js
+++ server/api/items.js
@@ -60,7 +60,6 @@ router.post(
         // dateListed,
       } = req.body
 
-      console.log('req.body', req.body)
       console.log('req.body.image', imageArr)
 
       //create new item data in item table -- working as of 3.20.21
@@ -73,35 +72,19 @@ router.post(
         userId,
       })
 
-      // await ItemPhoto.create({
-      //   cloudRef: `/images/${fileName}`,
-      //   downloadUrl: url,
-      // })
+      console.log(newItem)
 
-      // imageFiles upload to DB  -- working as of 3.20.21
+      imageArr.forEach(async (element) => {
+        console.log(element)
 
-      // if (req.files) {
-      //   //** */ the yellow lines are from : eslint-disable-next-line guard-for-in >>need to discuss with team
+        const itemPhotos = await ItemPhoto.create({
+          photoTitle: element.photoTitle,
+          cloudREF: element.cloudRef,
+          downloadURL: element.downloadUrl,
+        })
 
-      //   for (let key in req.files) {
-      //     const fileName = key
-
-      //     console.log('req.files', req.files, 'filename', req.files[fileName])
-
-      //     await ItemPhoto.create({
-      //       photoTitle: req.files[key].name,
-      //       photoFile: req.files[key].data,
-      //     })
-
-      //     //yf 03.22.21  associating pic to newItem
-      //     await itemPic.setItem(newItem)
-      //   }
-      // }
-
-      /**  THIS STILL NEEDS TO WORK - CURRENTLY NO ITEM AND ITEMPHOTO ASSOCIATION 03/21/21 */
-      // using magic method to associate the photo with the item
-      // createItem and setItem are not valid methods
-      //await newItem.addItemPhoto()
+        await itemPhotos.setItem(newItem)
+      })
 
       res.status(201).send(newItem)
     } catch (err) {
