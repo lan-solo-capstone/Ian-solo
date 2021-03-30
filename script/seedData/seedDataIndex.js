a713be43a94f1f9212a92abff27e8bac9fbba611
Yoshie Fujiwara <yoshie.fujiwara@gmail.com>
2021-03-30 11:44:11 -0500

    cleaned up channel and message models, seed data script and seed data files

--- script/seedData/seedDataIndex.js
+++ script/seedData/seedDataIndex.js
@@ -1,13 +1,9 @@
 const users = require('./userSeedData')
 const items = require('./itemSeedData')
 const itemPhotos = require('./itemPhotoSeedData')
-const channels = require('./channelsSeedData')
-const messages = require('./messagesSeedData')
 
 module.exports = {
   users,
   items,
   itemPhotos,
-  channels,
-  messages,
 }
