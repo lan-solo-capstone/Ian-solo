9ba479c304dce25d8f127f849dbe36e60b1e55e7
Jae Chung <jaepublic@gmail.com>
2021-03-30 20:40:27 -0500

    chore: add messages link

--- client/components/UserHome.js
+++ client/components/UserHome.js
@@ -51,6 +51,11 @@ class UserHome extends React.Component {
               </Link>
             </li>
           )}
+          {/* <li className="breadcrumb-item active" aria-current="page">
+            <Link to="/messages" className="text-decoration-none">
+              Messages
+            </Link>
+          </li> */}
           <li className="breadcrumb-item active" aria-current="page">
             <a
               className="text-decoration-none"
